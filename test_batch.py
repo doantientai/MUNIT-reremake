5301833568a7937cfde726a05c3c43b61a8d64f8
Tai Doan <t.doan@dental-monitoring.com>
2020-01-07 14:31:44 +0100

    update test batch

--- test_batch.py
+++ test_batch.py
@@ -3,7 +3,8 @@ Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 from __future__ import print_function
-from utils import get_config, get_data_loader_folder_for_test, pytorch03_to_pytorch04, load_inception
+# from utils import get_config, get_data_loader_folder_for_test, pytorch03_to_pytorch04, load_inception
+from utils import get_config, get_data_loader_folder_for_test, pytorch03_to_pytorch04
 # from trainer import MUNIT_Trainer, UNIT_Trainer
 from trainer import MUNIT_Trainer
 from torch import nn
@@ -14,14 +15,14 @@ from torch.autograd import Variable
 from data import ImageFolder
 import numpy as np
 import torchvision.utils as vutils
+
 try:
     from itertools import izip as zip
-except ImportError: # will be 3.x series
+except ImportError:  # will be 3.x series
     pass
 import sys
 import torch
 import os
-
 
 parser = argparse.ArgumentParser()
 # parser.add_argument('--config', type=str, default='configs/edges2handbags_folder', help='Path to the config file.')
@@ -37,27 +38,45 @@ parser.add_argument('--output_path', type=str, default='.', help="path for logs,
 parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
 parser.add_argument('--compute_IS', action='store_true', help="whether to compute Inception Score or not")
 parser.add_argument('--compute_CIS', action='store_true', help="whether to compute Conditional Inception Score or not")
-parser.add_argument('--inception_a', type=str, default='.', help="path to the pretrained inception network for domain A")
-parser.add_argument('--inception_b', type=str, default='.', help="path to the pretrained inception network for domain B")
-
+parser.add_argument('--inception_a', type=str, default='.',
+                    help="path to the pretrained inception network for domain A")
+parser.add_argument('--inception_b', type=str, default='.',
+                    help="path to the pretrained inception network for domain B")
 
 # parser.add_argument('--a2b', type=int, default=1, help="1 for a2b and 0 for b2a")
 # parser.add_argument('--input_folder', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn/testA')
 # parser.add_argument('--output_folder', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/output_test_batch/a2b', type=str, help="output image path")
+# parser.add_argument('--a2b', type=int, default=0, help="1 for a2b and 0 for b2a")
+# parser.add_argument('--input_folder', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn/testB')
+# parser.add_argument('--output_folder', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/output_test_batch/b2a', type=str, help="output image path")
+#
+# parser.add_argument('--config', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/checkpoints/config.yaml', type=str, help="net configuration")
+# parser.add_argument('--checkpoint', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/checkpoints/gen_00350000.pt', type=str, help="checkpoint of autoencoders")
+# parser.add_argument('--num_style', type=int, default=1, help="number of styles to sample")
+# parser.add_argument('--output_only', default=True, help="whether only save the output images or also save the input images")
 
+### test limited labels
+label = 0
 parser.add_argument('--a2b', type=int, default=0, help="1 for a2b and 0 for b2a")
-parser.add_argument('--input_folder', type=str, default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn/testB')
-parser.add_argument('--output_folder', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/output_test_batch/b2a', type=str, help="output image path")
-
-parser.add_argument('--config', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/checkpoints/config.yaml', type=str, help="net configuration")
-parser.add_argument('--checkpoint', default='/home/tai/Desktop/MUNIT-reremake-log/tmp6/MUNIT_CC_6losses/checkpoints/gen_00350000.pt', type=str, help="checkpoint of autoencoders")
-parser.add_argument('--num_style',type=int, default=1, help="number of styles to sample")
+parser.add_argument('--config', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC6l_LL1k_downloaded/outputs/mnist2svhn_002_infoStyle/config.yaml', type=str, help="net configuration")
+parser.add_argument('--num_style', type=int, default=1, help="number of styles to sample")
 parser.add_argument('--output_only', default=True, help="whether only save the output images or also save the input images")
+# parser.add_argument('--checkpoint', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC6l_LL1k_downloaded/outputs/mnist2svhn_002_infoStyle/checkpoints/gen_00080000.pt', type=str, help="checkpoint of autoencoders")
+# parser.add_argument('--output_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/trainA_1k+5k/9', type=str, help="output image path")
+# parser.add_argument('--checkpoint', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC6l_LL1k_downloaded/outputs/mnist2svhn_002_infoStyle/checkpoints/gen_00370000.pt', type=str, help="checkpoint of autoencoders")
+# parser.add_argument('--output_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/augmented_by_infoMUNIT/ckpt_370k/trainA_1k_5k/'+str(label), type=str, help="output image path")
+# parser.add_argument('--checkpoint', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC_6losses/checkpoints/gen_00350000.pt', type=str, help="checkpoint of autoencoders")
+# parser.add_argument('--output_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/augmented_by_infoMUNIT/MUNIT_CC6losses/ckpt_350k/trainA_60k_5k/'+str(label), type=str, help="output image path")
+# parser.add_argument('--checkpoint', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC_6losses/checkpoints/gen_00350000.pt', type=str, help="checkpoint of autoencoders")
+# parser.add_argument('--output_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/augmented_by_infoMUNIT/MUNIT_CC6losses/ckpt_350k/trainA_60k_10k/'+str(label), type=str, help="output image path")
 
+parser.add_argument('--checkpoint', default='/media/tai/6TB/Projects/InfoMUNIT/Models/MUNIT-reremake/MUNIT_CC6l_LL1k_downloaded/outputs/mnist2svhn_002_infoStyle/checkpoints/gen_00370000.pt', type=str, help="checkpoint of autoencoders")
+parser.add_argument('--output_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/augmented_by_infoMUNIT/MUNIT_CC6l_LL1k/ckpt_370k/trainA_0_5k/'+str(label), type=str, help="output image path")
 
+parser.add_argument('--input_folder', default='/media/tai/6TB/Projects/InfoMUNIT/Data/ForMUNIT/mnist2svhn_w_labels/trainB/'+str(label), type=str)
+LIMIT_SAMPLE = 500
 
 opts = parser.parse_args()
-
 
 torch.manual_seed(opts.seed)
 torch.cuda.manual_seed(opts.seed)
@@ -65,15 +84,6 @@ torch.cuda.manual_seed(opts.seed)
 # Load experiment setting
 config = get_config(opts.config)
 input_dim = config['input_dim_a'] if opts.a2b else config['input_dim_b']
-
-# Load the inception networks if we need to compute IS or CIIS
-if opts.compute_IS or opts.compute_IS:
-    inception = load_inception(opts.inception_b) if opts.a2b else load_inception(opts.inception_a)
-    # freeze the inception models and set eval mode
-    inception.eval()
-    for param in inception.parameters():
-        param.requires_grad = False
-    inception_up = nn.Upsample(size=(299, 299), mode='bilinear')
 
 # Setup model and data loader
 image_names = ImageFolder(opts.input_folder, transform=None, return_paths=True)
@@ -99,79 +109,32 @@ except:
 
 trainer.cuda()
 trainer.eval()
-encode = trainer.gen_a.encode if opts.a2b else trainer.gen_b.encode # encode function
-decode = trainer.gen_b.decode if opts.a2b else trainer.gen_a.decode # decode function
-
-if opts.compute_IS:
-    IS = []
-    all_preds = []
-if opts.compute_CIS:
-    CIS = []
+encode = trainer.gen_a.encode if opts.a2b else trainer.gen_b.encode  # encode function
+decode = trainer.gen_b.decode if opts.a2b else trainer.gen_a.decode  # decode function
 
 if opts.trainer == 'MUNIT':
     # Start testing
     style_fixed = Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(), volatile=True)
     for i, (images, names) in enumerate(zip(data_loader, image_names)):
-        if opts.compute_CIS:
-            cur_preds = []
+        if i >= LIMIT_SAMPLE:
+            break
         print(names[1])
         images = Variable(images.cuda(), volatile=True)
         content, _ = encode(images)
-        style = style_fixed if opts.synchronized else Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(), volatile=True)
+        style = style_fixed if opts.synchronized else Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda(),
+                                                               volatile=True)
         for j in range(opts.num_style):
             s = style[j].unsqueeze(0)
             outputs = decode(content, s)
             outputs = (outputs + 1) / 2.
-            if opts.compute_IS or opts.compute_CIS:
-                pred = F.softmax(inception(inception_up(outputs)), dim=1).cpu().data.numpy()  # get the predicted class distribution
-            if opts.compute_IS:
-                all_preds.append(pred)
-            if opts.compute_CIS:
-                cur_preds.append(pred)
             # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
             basename = os.path.basename(names[1])
-            path = os.path.join(opts.output_folder+"_%02d"%j,basename)
+            # path = os.path.join(opts.output_folder+"_%02d"%j,basename)
+            path = os.path.join(opts.output_folder, f'{basename[:-4]}_{j}{basename[-4:]}')
             if not os.path.exists(os.path.dirname(path)):
                 os.makedirs(os.path.dirname(path))
             vutils.save_image(outputs.data, path, padding=0, normalize=True)
-        # if opts.compute_CIS:
-        #     cur_preds = np.concatenate(cur_preds, 0)
-        #     py = np.sum(cur_preds, axis=0)  # prior is computed from outputs given a specific input
-        #     for j in range(cur_preds.shape[0]):
-        #         pyx = cur_preds[j, :]
-        #         CIS.append(entropy(pyx, py))
         if not opts.output_only:
             # also save input images
-            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
-    # if opts.compute_IS:
-    #     all_preds = np.concatenate(all_preds, 0)
-    #     py = np.sum(all_preds, axis=0)  # prior is computed from all outputs
-    #     for j in range(all_preds.shape[0]):
-    #         pyx = all_preds[j, :]
-    #         IS.append(entropy(pyx, py))
-
-    if opts.compute_IS:
-        print("Inception Score: {}".format(np.exp(np.mean(IS))))
-    if opts.compute_CIS:
-        print("conditional Inception Score: {}".format(np.exp(np.mean(CIS))))
-
-elif opts.trainer == 'UNIT':
-    # Start testing
-    for i, (images, names) in enumerate(zip(data_loader, image_names)):
-        print(names[1])
-        images = Variable(images.cuda(), volatile=True)
-        content, _ = encode(images)
-
-        outputs = decode(content)
-        outputs = (outputs + 1) / 2.
-        # path = os.path.join(opts.output_folder, 'input{:03d}_output{:03d}.jpg'.format(i, j))
-        basename = os.path.basename(names[1])
-        path = os.path.join(opts.output_folder,basename)
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
-        vutils.save_image(outputs.data, path, padding=0, normalize=True)
-        if not opts.output_only:
-            # also save input images
-            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0, normalize=True)
-else:
-    pass
+            vutils.save_image(images.data, os.path.join(opts.output_folder, 'input{:03d}.jpg'.format(i)), padding=0,
+                              normalize=True)
