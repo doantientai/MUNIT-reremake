ea98363d25b07e1885ea5b615257604fd4749c6d
Tai Doan <doantientai@gmail.com>
2020-09-11 11:56:30 +0200

    fix dimension of mu and var when len i = 1

--- trainer.py
+++ trainer.py
@@ -22,6 +22,10 @@ class NormalNLLLoss:
     """
 
     def __call__(self, x, mu, var):
+        if mu.dim() == 1 and var.dim() == 1:
+            mu = mu.unsqueeze(-1)
+            var = var.unsqueeze(-1)
+
         logli = -0.5 * (var.mul(2 * np.pi) + 1e-6).log() - (x - mu).pow(2).div(var.mul(2.0) + 1e-6)
         nll = -(logli.sum(-1).mean())
 
